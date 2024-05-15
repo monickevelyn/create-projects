7c1dab368a1c14cce72ba8e0f12840ad54c13d44
Monick Marinho <monickmarinhoc@gmail.com>
2024-05-15 09:08:48 -0300

    router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 
+import Home from './pages/home/Home.jsx'
+import Company from './pages/Company.jsx'
+import Contact from './pages/Contact.jsx'
+import NewProjects from './pages/new-project/NewProject.jsx'
+import Project from "./pages/project/Project.jsx"
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const rotas = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/",  element: <Home />},
+      { path: "company", element: <Company /> },
+      { path: "contact", element: <Contact />  },
+      { path: "newprojects",  element: <NewProjects />  },
+      { path: "project",  element: <Project />  }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={rotas} />  
+  </React.StrictMode>
 )
