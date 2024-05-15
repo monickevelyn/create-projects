762366d2933837cde9e199a5895260123b56fdb4
Monick Marinho <monickmarinhoc@gmail.com>
2024-05-15 07:56:41 -0300

    company-contact

---
+++ src/pages/Company.jsx
@@ -0,0 +1,14 @@
+import "./company-contact.scss"
+
+const Company = () => {
+  return (
+    <>
+    <div className="page">
+        <h1>Empresa</h1>
+        <p>Página de representação.</p>
+    </div>
+    </>
+  )
+}
+
+export default Company;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import "./company-contact.scss"
+
+const Contact = () => {
+  return (
+    <>
+    <div className="page">
+        <h1>Contato</h1>
+        <p>Página de representação.</p>
+    </div>
+    </>
+  )
+}
+
+export default Contact;
