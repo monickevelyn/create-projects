6cefd8561f6aecf061b372eb7cb092a81930368d
Monick Marinho <monickmarinhoc@gmail.com>
2024-05-15 08:14:41 -0300

    new-project

---
+++ src/pages/new-project/NewProject.jsx
@@ -0,0 +1,13 @@
+import './new-project.scss'
+
+const NewProjects = () => {
+    return ( 
+        <>
+        <section className='new-project-container'>
+            <h1>Criar Projeto</h1>
+            <p>Crie o seu projeto para depois adicionar os serviços</p>
+        </section>
+        </>
+    );
+}
+export default NewProjects;
