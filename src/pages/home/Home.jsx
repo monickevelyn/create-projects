b296cdecead3480b785bb97eac0bd01da25933a2
Monick Marinho <monickmarinhoc@gmail.com>
2024-05-15 08:06:50 -0300

    home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import './home.scss'
+
+const Home = () => {
+    return ( 
+        <>
+         <section className='home-container'>
+            <h1>Bem-vindo ao <span>Projects</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <a href="" className="link-btn" >Criar Projeto</a>
+         </section>
+        </>
+    );
+}
+export default Home;
